02935061efe03567bd3841a05eb5a69f32f4f7fc
Gleb Billig <gleb.billig@gmail.com>
2019-03-09 08:11:35 -0500

    Add utility to replace month strings to numbers

--- downloader.py
+++ downloader.py
@@ -19,18 +19,18 @@ ark_id_hash = {}
 date_hash = {}
 
 month_sub = {
-  " janvier ": "/01/",
-  " février ": "/02/",
-  " mars ": "/03/",
-  " avril ": "/04/",
-  " mai ": "/05/",
-  " juin ": "/06/",
-  " juillet ": "/07/",
-  " août ": "/08/",
-  " septembre ": "/09/",
-  " octobre ": "/10/",
-  " novembre ": "/11/",
-  " décembre ": "/12/",
+  " janvier ": "-01-",
+  " février ": "-02-",
+  " mars ": "-03-",
+  " avril ": "-04-",
+  " mai ": "-05-",
+  " juin ": "-06-",
+  " juillet ": "-07-",
+  " août ": "-08-",
+  " septembre ": "-09-",
+  " octobre ": "-10-",
+  " novembre ": "-11-",
+  " décembre ": "-12-",
 }
 
 if not os.path.isfile(ark_database_filename) or not os.path.isfile(date_database_filename):
--- util.py
+++ util.py
@@ -1,16 +1,46 @@
 import json
 
 
-with open('articles.json', 'rb') as fp:
-  articles = json.load(fp)
+with open('ark.json', 'rb') as fp:
+  ark_id_hash = json.load(fp)
 
-for article in articles:
-  if 'é' in article["date"]:
-    article['date'] = article['date'].replace('é', 'e')
+with open('date.json', 'rb') as fp:
+  date_hash = json.load(fp)
 
-  if 'û' in article["date"]:
-    article['date'] = article['date'].replace('û', 'u')
+if False:
+  for article in articles:
+    if 'é' in article["date"]:
+      article['date'] = article['date'].replace('é', 'e')
 
-with open('articles_nice.json', 'w', encoding='utf8') as fp:
-  json.dump(articles, fp, sort_keys=True, indent=4)
+    if 'û' in article["date"]:
+      article['date'] = article['date'].replace('û', 'u')
+
+
+month_sub = {
+  " janvier ": "-01-",
+  " février ": "-02-",
+  " fevrier ": "-02-",
+  " mars ": "-03-",
+  " avril ": "-04-",
+  " mai ": "-05-",
+  " juin ": "-06-",
+  " juillet ": "-07-",
+  " août ": "-08-",
+  " aout ": "-08-",
+  " septembre ": "-09-",
+  " octobre ": "-10-",
+  " novembre ": "-11-",
+  " décembre ": "-12-",
+  " decembre ": "-12-",
+}
+
+for ark_id, date in date_hash.items():
+  for month_spelled, month_numerical in month_sub.items():
+    if month_spelled in date:
+      date_hash[ark_id] = date.replace(month_spelled, month_numerical)
+      break
+
+
+with open('date_nice.json', 'w', encoding='utf8') as fp:
+  json.dump(date_hash, fp, sort_keys=True, indent=4)
 
